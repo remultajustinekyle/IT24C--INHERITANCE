2274385edc193585d420807f7a336c8a3988762b
remultajustinekyle <justinekyleremulta2@gmail.com>
2023-11-10 22:50:02 +0800

    added Cat class

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,13 @@
 
 public class Cat extends sarah {
     
+    Cat (String name){
+        super(name);
+    }
+    
+    void sleep(){
+        System.out.println(name + " is sleeping");
+        
+    }
     
 }
